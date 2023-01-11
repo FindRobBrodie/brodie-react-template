9bc3afffbdf249874bb08a1d16e672195eec30e3
Rob Brodie <54519499+FindRobBrodie@users.noreply.github.com>
2023-01-11 14:42:17 -0500

    Delete BrodieReactTemplate directory

--- BrodieReactTemplate/ClientApp/src/routes/_app/NoMatch404Route.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@mui/material'
-
-export const NoMatch404Route = () => {
-  return (
-    <Container maxWidth='xl' sx={{pt: 5}}>
-      Something went wrong!  There is no page at this location.  Please use the menu above to navigate to another page.      
-    </Container>
-  )
-}
--- BrodieReactTemplate/ClientApp/src/typeDefinitions/appTypes.ts
+++
@@ -1,6 +0,0 @@
-/* Use this file to define any app wide type definitions */
-
-export type LooseObject = {
-  [key: string]: any
-}
-
